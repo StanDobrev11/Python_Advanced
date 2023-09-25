75d6f6a680ad06a8dce817a66deae8664ff63f0f
StanDobrev11 <dobrev81@gmail.com>
2023-09-25 21:20:33 +0000

    'update'

--- 00_Exams/04_Python_Advanced_Retake_Exam_-_16_December_2020/03_magic_triangle.py
+++ 00_Exams/04_Python_Advanced_Retake_Exam_-_16_December_2020/03_magic_triangle.py
@@ -19,3 +19,30 @@ Test Code                               Output
 get_magic_triangle(5)
                                         [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
 """
+
+
+# def get_magic_triangle(rows):
+#     mtrx = [[1] for _ in range(rows)]
+#     for row in range(1, rows):
+#         col = row
+#         for cur_c in range(1, col + 1):
+#             try:
+#                 cell = mtrx[row - 1][cur_c] + mtrx[row - 1][cur_c - 1]
+#             except IndexError:
+#                 cell = 1
+#             mtrx[row].append(cell)
+#
+#     return mtrx
+
+
+def get_magic_triangle(n):
+    mtrx = [[1], [1, 1]]
+    for row in range(2, n):
+        mtrx.append([1] * (row + 1))
+        for col in range(1, row):
+            mtrx[row][col] = mtrx[row - 1][col] + mtrx[row - 1][col - 1]
+
+    return mtrx
+
+
+get_magic_triangle(5)
