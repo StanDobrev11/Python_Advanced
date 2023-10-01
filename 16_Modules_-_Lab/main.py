b7c279b1d36e0376eec755ac977f88f4ae5fac0f
StanDobrev11 <dobrev81@gmail.com>
2023-10-01 18:36:15 +0000

    update

--- 16_Modules_-_Lab/main.py
+++ 16_Modules_-_Lab/main.py
@@ -3,15 +3,18 @@ from lab_works.fibonacci import create_sequence, locate
 
 # print(calculate_string('2.5 * 3'))
 
-
-
-data = input()
-
+seq = []
 command = input()
 while command != 'Stop':
-    data = data.split()
-    number = int(data.pop())
-    func = "_".join(data)
-    
+
+    data = command.split()
+    if data[0].lower() == 'create':
+        count = int(data[-1])
+        seq = create_sequence(count)
+        print(' '.join(map(str, seq)))
+
+    elif data[0].lower() == 'locate':
+        number = int(data[-1])
+        print(locate(number, seq))
 
     command = input()
